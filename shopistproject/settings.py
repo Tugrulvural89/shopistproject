a240f047fe6a83296c3651b66c326c956e3d6861
tugrulvural89 <tugrulv89@gmail.com>
2018-12-02 02:12:21 +0000

    cronupdate

--- shopistproject/settings.py
+++ shopistproject/settings.py
@@ -20,7 +20,7 @@ STATIC_DIR = os.path.join(BASE_DIR,  'static')
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'qky$xd67eh==_&sjw*69c^nuyi70g5l&pz52@##&pr%g&ko2@b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
