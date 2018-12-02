88847cfb045b6ef0f7249904c6995de08afac3b0
Tugrulvural89 <tugrulv89@gmail.com>
2018-12-02 05:42:03 +0300

    cron4bs4sets

--- shopistproject/settings.py
+++ shopistproject/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'Shopist.apps.ShopistConfig',
-    'Shopist'
+    'Shopist',
     'bs4cron',
 
 ]
