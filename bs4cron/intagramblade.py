dd10809448eebf12bebd260235939f4166b72659
Tugrulvural89 <tugrulv89@gmail.com>
2018-12-26 00:23:18 +0300

    instagramblade try listindex error

--- bs4cron/intagramblade.py
+++ bs4cron/intagramblade.py
@@ -32,17 +32,26 @@ for row in ws.rows:
         tagpic = data['entry_data']['TagPage'][0]['graphql']['hashtag']['profile_pic_url']
         hastag = hastag
         for post in data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges']:
-            intagramcontent.append({
-                'image_src': post['node']['thumbnail_resources'][2]['src'],
-                'image_comment' : post['node']['edge_media_to_caption']['edges'][0]['node']['text'],
-                'image_display_url':post['node']['display_url'],
-                'image_like_count':int(post['node']['edge_liked_by']['count']),
-                'tagname':tagname,
-                'tagpic':tagpic,
-                'hastag':hastag,
-            })
-
-
+            try:
+                intagramcontent.append({
+                    'image_src': post['node']['thumbnail_resources'][2]['src'],
+                    'image_comment': post['node']['edge_media_to_caption']['edges'][0]['node']['text'],
+                    'image_display_url': post['node']['display_url'],
+                    'image_like_count': int(post['node']['edge_liked_by']['count']),
+                    'tagname': tagname,
+                    'tagpic': tagpic,
+                    'hastag': hastag,
+                })
+            except IndexError:
+                intagramcontent.append({
+                    'image_src': post['node']['thumbnail_resources'][2]['src'],
+                    'image_comment': "muhteşem resim!!",
+                    'image_display_url': post['node']['display_url'],
+                    'image_like_count': int(post['node']['edge_liked_by']['count']),
+                    'tagname': tagname,
+                    'tagpic': tagpic,
+                    'hastag': hastag,
+                })
 
 for objectitem in intagramcontent:
 
